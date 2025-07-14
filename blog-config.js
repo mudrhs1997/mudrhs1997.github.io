c77db0cd896b63152d5f83982e067ee3efc97084
mudrhs1997 <mudrhs1997@daum.net>
2025-07-15 01:22:46 +0900

    docs: 고정폭 글꼴

--- blog-config.js
+++ blog-config.js
@@ -2,18 +2,18 @@ module.exports = {
   title: "myunggon",
   description: "책장",
   author: "mason",
-  siteUrl: "https://mudrhs1997.github.io/gatsby-starter-hoodie/",
+  siteUrl: "https://mudrhs1997.github.io/",
   links: {
-    github: "https://github.com/devHudi",
+    github: "https://github.com/mudrhs1997",
     medium: "https://medium.com",
     email: "mudrhs1997@naver.com",
-    resume: "https://hudi.blog",
-    link: "https://hudi.blog",
+    resume: "https://github.com/mudrhs1997",
+    link: "https://github.com/mudrhs1997",
   },
   useAbout: true,
   // See https://giscus.app/
   giscus: {
-    repo: "devHudi/gatsby-starter-hoodie",
+    repo: "mudrhs1997/mudrhs1997.github.io",
     repoId: "MDEwOlJlcG9zaXRvcnkzNjk4NjMzNTg=",
     category: "Comments",
     categoryId: "DIC_kwDOFguqvs4ChwGy",
